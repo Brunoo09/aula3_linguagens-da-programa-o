358b6c10c109cb79f51a6c25a6de4650f355b35f
Brunoo09 <fonsecacarneirobruno@gmail.com>
2024-10-18 17:15:30 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,62 @@
+'''#floats numero reais, com ponto
+user_name = 'brunao'
+real1 = 25.9
+real2 = 0.
+real3 = .0
+
+#integer(int)
+numero_inteiro1 = 10
+numero_inteiro2 = -10
+numero_inteiro3 = 0
+
+#função - ação
+
+print('mostrar algo no console')
+numero = 10
+boleana= True
+real = 5.90
+text = 'vai chover'
+
+tipo = type(numero)
+tipo2 = type(text)
+tipo3 = type(boleana)
+tipo4 = type(real)
+print(tipo)
+print(tipo2)
+print(tipo3)
+print(tipo4)
+
+tipo = type(numero)
+print(tipo)
+
+#----------------------------------------------------
+
+inteiro_em_real = float(numero)
+
+'''
+#atvidade
+
+n1 = int(input('='))
+n2 = int(input('='))
+soma = n1 + n2
+print(soma)
+
+
+#subtração
+n1 = int(input('='))
+n2 = int(input('='))
+subtração = n1 - n2
+print(subtração)
+
+#multiplicação
+n1 = int(input('='))
+n2 = int(input('='))
+multiplicação = n1 * n2
+print(multiplicação)
+
+#divisão
+n1 = float(input('='))
+n2 = float(input('='))
+divisão = n1 / n2
+print(divisão)
+
